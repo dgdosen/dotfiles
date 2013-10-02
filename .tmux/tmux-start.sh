021780d2320770f7e2f3e5a83e1726d4ca15fbbf
Daniel Dosen <ddosen@gmail.com>
2013-10-01 23:08:25 -0700

    using tmux percentages

--- .tmux/tmux-start.sh
+++ .tmux/tmux-start.sh
@@ -1,18 +1,23 @@
-tmux new-session -d -s $1 
+tmux new-session -d -s $1
+
 tmux send-keys -t $1 "c $1" C-m
 tmux send-keys 'vim' C-m
-tmux split-window -h
+
+tmux split-window -h -p 30
 tmux send-keys -t $1 "c $1" C-m
 tmux send-keys 'zeus start' C-m
-tmux resize-pane -R 20
-tmux splitw -v 
-tmux resize-pane -U 17
+
+tmux splitw -v -p 90
 tmux send-keys -t $1 "c $1" C-m
-tmux send-keys 'zeus s' C-m
-tmux splitw -v
+tmux send-keys 'rails s' C-m
+
+tmux splitw -v -p 66
 tmux send-keys -t $1 "c $1" C-m
 tmux send-keys 'guard' C-m
-tmux select-pane -L 
+
+tmux splitw -v
+tmux send-keys -t $1 "c $1" C-m
+tmux send-keys 'karma start spec/karma/config/unit.js' C-m
+
+tmux select-pane -L
 tmux attach-session -d
-
-
