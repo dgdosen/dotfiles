1d303b7a819174f39fcc3a463cb7b32e1c7b0272
Daniel Dosen <dgdosen@gmail.com>
2016-03-12 13:19:14 -0800

    changing db to project_b

--- postgres_project_b_reset.sh
+++ postgres_project_b_reset.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo dropping database
-dropdb -U postgres -h localhost foobar
+dropdb -U postgres -h localhost project_b
 echo creating database
-createdb -U postgres -h localhost foobar
+createdb -U postgres -h localhost project_b
 
 echo restoring
 date
 time
 file=$(ls -t /Users/ddosen/Dropbox/joined_shares/project_b_share/pg_database_backup/project_b* | head -1)
 echo $file
-pg_restore $file -U postgres -d foobar -h localhost -j 3 -Fc > /dev/null
+pg_restore $file -U postgres -d project_b -h localhost -j 3 -Fc > /dev/null
 date
 echo finished restoring
