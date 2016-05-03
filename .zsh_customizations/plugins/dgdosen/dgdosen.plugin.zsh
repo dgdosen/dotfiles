ef185892c97243709d4fd599ed3fd31af65ebeb4
Daniel Dosen <dgdosen@gmail.com>
2016-05-03 15:26:58 -0700

    tps reporting updates

--- .zsh_customizations/plugins/dgdosen/dgdosen.plugin.zsh
+++ .zsh_customizations/plugins/dgdosen/dgdosen.plugin.zsh
@@ -1,6 +1,6 @@
 c() { cd ~/Development/$1; }
 _c() { _files -W ~/Development -/; }
 compdef _c c
-tps() { cd ~/pdropbox/_my.documents/data.business/dosen_financial/customers/project_b/time/$1; }
-_tps() { _files -W ~/pdropbox/_my.documents/data.business/dosen_financial/customers/project_b/time -/; }
+tps() { cd ~/pdropbox/_my.documents/data.business/dosen_financial/time/$1; }
+_tps() { _files -W ~/pdropbox/_my.documents/data.business/dosen_financial/time -/; }
 compdef _tps tps
