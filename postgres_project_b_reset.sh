0895aa2593d822cbfa621d94aa4d945034a4ed9e
Daniel Dosen <dgdosen@gmail.com>
2016-08-03 11:17:16 -0700

    postgres reset update

--- postgres_project_b_reset.sh
+++ postgres_project_b_reset.sh
@@ -7,7 +7,7 @@ createdb -U postgres -h localhost project_b
 echo restoring
 date
 time
-file=$(ls -t /Users/ddosen/Dropbox/joined_shares/project_b_share/pg_database_backup/project_b* | head -1)
+file=$(ls -t /Users/ddosen/dropboxm/joined_shares/project_b_share/pg_database_backup/project_b* | head -1)
 echo $file
 pg_restore $file -U postgres -d project_b -h localhost -j 3 -Fc > /dev/null
 date
