c21a96d92c032f4c5950fb724409f39b2ab2301b
Daniel Dosen <dgdosen@gmail.com>
2016-12-05 08:21:21 -0800

    changing dev structure

--- .zsh_customizations/plugins/dgdosen/dgdosen.plugin.zsh
+++ .zsh_customizations/plugins/dgdosen/dgdosen.plugin.zsh
@@ -1,5 +1,5 @@
-c() { cd ~/Development/$1; }
-_c() { _files -W ~/Development -/; }
+c() { cd ~/dev/$1; }
+_c() { _files -W ~/dev -/; }
 compdef _c c
 tps() { cd ~/pdropbox/_my.documents/data.business/dosen_financial/time/$1; }
 _tps() { _files -W ~/pdropbox/_my.documents/data.business/dosen_financial/time -/; }
