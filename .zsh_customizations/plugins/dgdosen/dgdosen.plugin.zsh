a23f2cafca4184a92657734dfe58eda1b84fb457
Daniel Dosen <dgdosen@gmail.com>
2016-12-05 22:19:10 -0800

    groom alias and completion commands

--- .zsh_customizations/plugins/dgdosen/dgdosen.plugin.zsh
+++ .zsh_customizations/plugins/dgdosen/dgdosen.plugin.zsh
@@ -1,6 +1,15 @@
 c() { cd ~/dev/$1; }
 _c() { _files -W ~/dev -/; }
+
 compdef _c c
-tps() { cd ~/pdropbox/_my.documents/data.business/dosen_financial/time/$1; }
+
+time_files() { cd ~/pdropbox/_my.documents/data.business/dosen_financial/time/$1; }
+_time_files() { _files -W ~/pdropbox/_my.documents/data.business/dosen_financial/time -/; }
+
+compdef _time_files time_files
+
+tps() { vim ~/pdropbox/_my.documents/data.business/dosen_financial/time/$1; }
 _tps() { _files -W ~/pdropbox/_my.documents/data.business/dosen_financial/time -/; }
+
 compdef _tps tps
+
