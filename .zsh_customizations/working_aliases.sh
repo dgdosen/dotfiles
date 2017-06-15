7a4dab0ea0e9daccc0ed40a44359e7ebfd00e4bd
Daniel Dosen <dgdosen@gmail.com>
2017-06-15 09:11:58 -0700

    mor shortcuts

---
+++ .zsh_customizations/working_aliases.sh
@@ -0,0 +1,13 @@
+# shortcut for ssh to makerboarding
+
+sshm () {
+  ssh deployer@makerboarding.com
+}
+
+ssha () {
+  ssh deployer@agidevelopment.com
+}
+
+sshr () {
+  ssh registrar@registry.agidevelopment.com
+}
