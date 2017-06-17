606fc5065d6872bb5e7be05d633542da16415e31
Daniel Dosen <dgdosen@gmail.com>
2017-06-17 10:34:15 -0700

    adding pomo customizations

---
+++ .zsh_customizations/pomo_aliases.sh
@@ -0,0 +1,21 @@
+# Short for docker-machine
+pi () {
+  pomo incomplete $@
+}
+
+pl () {
+  pomo list --all
+}
+
+pb () {
+  pomo break $@
+}
+
+pa () {
+  pomo add $@
+}
+
+ps () {
+  pomo start $@
+}
+
