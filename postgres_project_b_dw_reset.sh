28477660690cbcbdee97b411854b78e8005c07c5
Daniel Dosen <dgdosen@gmail.com>
2018-11-06 23:46:43 -0800

    adding pg dw backup

---
+++ postgres_project_b_dw_reset.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo dropping database
+dropdb -U postgres -h localhost project_b_dw_development
+echo creating database
+createdb -U postgres -h localhost project_b_dw_development
+
+echo restoring
+date
+time
+file=$(ls -t /Users/ddosen/dropboxm/joined_shares/project_b_share/pg_dw_database_backup/project_b* | head -1)
+echo $file
+pg_restore $file -U postgres -d project_b_dw_development -h localhost -j 3 -Fc > /dev/null
+date
+echo finished restoring
