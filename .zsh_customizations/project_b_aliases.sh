dde494ce5b22a4f908f7f35afd6528c171d0bd1a
Daniel Dosen <dgdosen@gmail.com>
2019-06-16 08:07:26 -0700

    adding shell alias command for project b tasks.

---
+++ .zsh_customizations/project_b_aliases.sh
@@ -0,0 +1,11 @@
+pbd () {
+
+  find ~/Downloads/project_b/data -type f -exec chmod -x {} \;
+  # for D in *
+  # do
+  #   # Do whatever you need with D
+  #   echo "hello ${D}"
+  #   chmod -x D {} \;
+  # done
+
+}
