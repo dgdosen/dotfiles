156dbf5216883d59596e42b1ddae7e732665ce73
Daniel Dosen <dgdosen@gmail.com>
2019-10-11 07:56:16 -0700

    wip cleanup up restore scripts

--- postgres_project_b_dw_reset.sh
+++ postgres_project_b_dw_reset.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo --
 echo dropping database
 dropdb -U postgres -h localhost project_b_dw_development
 echo creating database
@@ -6,7 +7,6 @@ createdb -U postgres -h localhost project_b_dw_development
 
 echo restoring
 date
-time
 file=$(ls -t /Users/ddosen/dropboxm/joined_shares/project_b_share/pg_dw_database_backup/project_b* | head -1)
 echo $file
 pg_restore $file -U postgres -d project_b_dw_development -h localhost -j 3 -Fc > /dev/null
