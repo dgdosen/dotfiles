ed200e1c100d2e1afc53238766bf8fe90d152a34
Daniel Dosen <dgdosen@gmail.com>
2019-12-07 08:50:32 -0800

    wip: updating how dotfiles are managed

---
+++ dot_install.sh
@@ -0,0 +1,7 @@
+# scripting test - link files based on shell type?
+# - link files based on all files?
+# - link files based on folders and environment!
+
+
+# sample
+ln -sv ~/.dotfiles/foobar.txt ~/.foobar.txt
