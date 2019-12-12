ef3a99deb2f7fde74b65538f6840633ff0b2af0c
Daniel Dosen <dgdosen@gmail.com>
2019-12-11 18:42:04 -0800

    submodule link updates

--- submodule_install.sh
+++ submodule_install.sh
@@ -1,5 +1,5 @@
-git submodule add git@github.com/robbyrussell/oh-my-zsh.git ~/.dotfiles/.oh-my-zsh
+git submodule add https://github.com/ohmyzsh/ohmyzsh.git ~/.dotfiles/.ohmyzsh
 git submodule add https://github.com/zsh-users/zsh-autosuggestions
 ~/.dotfiles/.zsh_customizations/plugins/zsh-autosuggestions
-git submodule add https://github.com/bhilburn/powerlevel9k.git
+git submodule add https://github.com/Powerlevek9k/powerlevel9k.git
 ~/.dotfiles/.zsh_customizations/themes/powerlevel9k
