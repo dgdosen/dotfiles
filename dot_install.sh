cb8954b7041b14af91ae49f2a1b73d31fefcb50c
Daniel Dosen <dgdosen@gmail.com>
2019-12-11 17:52:05 -0800

    wip dot_install

--- dot_install.sh
+++ dot_install.sh
@@ -9,3 +9,23 @@ ln -sv ~/.dotfiles/postgres_project_b_reset.sh ~/.postgres_project_b_reset.sh
 ln -sv ~/.dotfiles/postgres_project_b_dw_reset.sh ~/.postgres_project_b_dw_reset.sh
 ln -sv ~/.dotfiles/.zsh ~/.zsh
 ln -sv ~/.dotfiles/.zshrc ~/.zshrc
+ln -sv ~/.dotfiles/.zsh_customizations ~/.zsh_customizations
+
+ln -sv ~/.dotfiles/.vimrc ~/.vimrc
+ln -sv ~/.dotfiles/.vim_nvim~/.vim_nvim
+ln -sv ~/.dotfiles/.vim ~/.vim
+
+ln -sv ~/.dotfiles/.tmux ~/.tmux
+ln -sv ~/.dotfiles/.tmuxinator ~/.tmuxinator
+ln -sv ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sv ~/.dotfiles/.tmux-theme-gruvbox.conf ~/.tmux-theme-gruvbox.conf
+
+ln -sv ~/.dotfiles/.rbuy-version ~/.ruby-version
+ln -sv ~/.dotfiles/.gemrc ~/.gemrc
+ln -sv ~/.dotfiles/.pryrc ~/.pryrc
+ln -sv ~/.dotfiles/.prettierrc ~/.prettierrc
+
+ln -sv ~/.dotfiles/.gitconfg ~/.gitconfig
+ln -sv ~/.dotfiles/.bin ~/.bin
+
+ln -sv ~/.dotfiles/.vim_nvim ~/.config/nvim
