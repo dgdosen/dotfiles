288c1cc764083286b8bbce767d7a3fb4f8801f89
Daniel Dosen <dgdosen@gmail.com>
2019-12-11 17:42:54 -0800

    wip dot_install

--- dot_install.sh
+++ dot_install.sh
@@ -5,3 +5,7 @@
 
 # sample
 ln -sv ~/.dotfiles/foobar.txt ~/.foobar.txt
+ln -sv ~/.dotfiles/postgres_project_b_reset.sh ~/.postgres_project_b_reset.sh
+ln -sv ~/.dotfiles/postgres_project_b_dw_reset.sh ~/.postgres_project_b_dw_reset.sh
+ln -sv ~/.dotfiles/.zsh ~/.zsh
+ln -sv ~/.dotfiles/.zshrc ~/.zshrc
