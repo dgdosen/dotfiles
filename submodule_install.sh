413ba3002b49fc4f5bd3da410bf09b2547f875fc
Daniel Dosen <dgdosen@gmail.com>
2019-12-11 18:30:18 -0800

    tweak in submodule installation

--- submodule_install.sh
+++ submodule_install.sh
@@ -1,5 +1,5 @@
-git submodule add git://github.com/robbyrussell/oh-my-zsh.git
+git submodule add git@github.com/robbyrussell/oh-my-zsh.git ~/.dotfiles/.oh-my-zsh
 git submodule add https://github.com/zsh-users/zsh-autosuggestions
-.zsh_customizations/plugins/zsh-autosuggestions
-git submodule add https://github.com/bhilburn/powerlevel0k.git
-zsh_customizations/themes/powerlevel9k
+~/.dotfiles/.zsh_customizations/plugins/zsh-autosuggestions
+git submodule add https://github.com/bhilburn/powerlevel9k.git
+~/.dotfiles/.zsh_customizations/themes/powerlevel9k
