ce12c51bd68827747c646d8a65fdf2ce6810fb8a
Daniel Dosen <dgdosen@gmail.com>
2019-12-11 18:19:52 -0800

    wip submodule install

---
+++ submodule_install.sh
@@ -0,0 +1,5 @@
+git submodule add git://github.com/robbyrussell/oh-my-zsh.git
+git submodule add https://github.com/zsh-users/zsh-autosuggestions
+.zsh_customizations/plugins/zsh-autosuggestions
+git submodule add https://github.com/bhilburn/powerlevel0k.git
+zsh_customizations/themes/powerlevel9k
