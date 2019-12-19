f387c8c624bd9fa8d69ee6358d8e51386e2ab8b4
Daniel Dosen <dgdosen@gmail.com>
2019-12-19 14:53:34 -0600

    updates to common project db reset

--- postgres_project_b_reset.sh
+++ postgres_project_b_reset.sh
@@ -7,7 +7,7 @@ createdb -U postgres -h localhost project_b_development
 
 echo restoring
 date
-file=$(ls -t /Users/ddosen/dropboxm/joined_shares/project_b_share/pg_database_backup/project_b* | head -1)
+file=$(ls -t $HOME/dropboxm/joined_shares/project_b_share/pg_database_backup/project_b* | head -1)
 echo $file
 pg_restore $file -U postgres -d project_b_development -h localhost -j 3 -Fc > /dev/null
 date
