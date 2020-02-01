4d6920978a64d0d689cc7b561948f43bbe482a7a
Daniel Dosen <dgdosen@gmail.com>
2020-02-01 10:34:51 -0800

    moved fetch daily file, and into a function

---
+++ .zsh_customizations/daily/fetch_daily.sh
@@ -0,0 +1,31 @@
+fetchthegits() {
+
+  FOO=$PWD
+
+  cd ~/.dotfiles
+  git fetch
+  cd ~/dev/project_b
+  git fetch
+  cd ~/dev/project_b_data
+  git fetch
+  cd ~/dev/project_b_racing_form
+  git fetch
+  cd ~/dev/project_b_debut
+  git fetch
+  cd ~/dev/project_b_pars
+  git fetch
+  cd ~/dev/project_b_generic_matching
+  git fetch
+  cd ~/dev/project_b_matching
+  git fetch
+  cd ~/dev/sessuru_material
+  git fetch
+  cd ~/dev/sessuru_server
+  git fetch
+  cd ~/dev/sessuru_client
+  git fetch
+
+  cd $FOO
+
+}
+
