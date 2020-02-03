53e010ce119620adefca5401e002fa157e8b2ba1
Daniel Dosen <dgdosen@gmail.com>
2020-02-03 07:20:49 -0800

    tweaking sp shell script

--- .postgres/sp_data.sh
+++ .postgres/sp_data.sh
@@ -7,12 +7,4 @@ psql \
     -f $HOME/.dotfiles/.postgres/sp_data.sql \
     project_b_development
 
-# psql_exit_status = $?
-
-# if [ $psql_exit_status != 0 ]; then
-#     echo "psql failed while trying to run this sql script" 1>&2
-#     exit $psql_exit_status
-# fi
-
-echo "sql script successful"
 exit 0
