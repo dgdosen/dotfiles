5c6e1fd7405de3c89c434c02a4f1aa38e3cc11e2
Daniel Dosen <dgdosen@gmail.com>
2020-02-20 07:05:33 -0800

    cron support refactor

--- daily/cron_project_b_dw.sh
+++ daily/cron_project_b_dw.sh
@@ -5,4 +5,4 @@ PGDATA=$HOME/Library/Application\ Support/Postgres/var-11 pg_ctl stop
 PGDATA=$HOME/Library/Application\ Support/Postgres/var-11 pg_ctl start
 echo - updating project b db
 $HOME/.dotfiles/.postgres/postgres_project_b_dw_reset.sh
-touch ~/.cron_support/cron_project_b.txt
+touch ~/.cron_support/cron_project_b_dw.txt
