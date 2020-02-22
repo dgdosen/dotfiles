44fcadab8d574257f4c5f39a45e961de8537bca6
Daniel Dosen <dgdosen@gmail.com>
2020-02-22 05:26:52 -0800

    changing order of nightly update tasks

--- daily/cron_nightly_update.sh
+++ daily/cron_nightly_update.sh
@@ -9,7 +9,7 @@ $HOME/.dotfiles/daily/cronbrew.sh
 echo - updating important git repos
 $HOME/.dotfiles/daily/cronfetch.sh
 
-echo - updating project b db
-$HOME/.dotfiles/daily/cron_project_b.sh
 echo - updating project b dw db
 $HOME/.dotfiles/daily/cron_project_b_dw.sh
+echo - updating project b db
+$HOME/.dotfiles/daily/cron_project_b.sh
