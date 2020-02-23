0cdd56f26a5d2fce341b54512a8377f4cdec6818
Daniel Dosen <dgdosen@gmail.com>
2020-02-23 07:06:34 -0800

    wip: debugging cron plugin update

--- daily/crondot.sh
+++ daily/crondot.sh
@@ -1,4 +1,15 @@
 #!/usr/local/bin/zsh
 source ~/.zshrc
-dotup
+cd $HOME/.dotfiles
+echo - in dotfiles
+gsm
+echo - updated modules
+gsp
+echo - pulled modules
+nvim +'PlugInstall --sync' +qa
+echo - installed vim plugs
+nvim +'PlugUpdate --sync' +qa
+echo - updated vim plugs
+# nvim +'UpdateRemotePlugins' +qa
 touch ~/.cron_support/crondot.txt
+echo - touched dot
