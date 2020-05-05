c0b313d60e0744d939a2a60666b5b9af78e6dedf
Daniel Dosen <dgdosen@gmail.com>
2020-05-05 13:36:21 -0700

    wip: dotfile setup

--- setup/submodule_setup.sh
+++ setup/submodule_setup.sh
@@ -3,6 +3,5 @@ cd ~/.dotfiles
 git submodule add https://github.com/egel/tmux-gruvbox.git .tmux/plugins/tmux-gruvbox
 git submodule add https://github.com/tmux-plugins/tpm.git .tmux/plugins/tpm
 git submodule add https://github.com/zsh-users/zsh-autosuggestions.git .zsh_customizations/plugins/zsh-autosuggestions
-git submodule add https://github.com/bhilburn/powerlevel9k.git .zsh_customizations/themes/powerlevel9k
 # note = different on linux!
 # git submodule add https://github.com/zsh-users/zsh-syntax-highlighting.git .zsh_customizations/plugins/zsh-syntax-highlighting
