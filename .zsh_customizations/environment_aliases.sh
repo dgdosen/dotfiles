0778cb9dd43a6988cc35f934502cf443ba790501
Daniel Dosen <dgdosen@gmail.com>
2020-05-09 09:58:06 -0700

    wip: doing more with fzf

--- .zsh_customizations/environment_aliases.sh
+++ .zsh_customizations/environment_aliases.sh
@@ -30,6 +30,41 @@ brewup() {
   brew cleanup
 }
 
+fzffd() {
+ local dir
+ dir=$(find ${1:-.} -path '*/\.*' -prune -o -type d \
+      -print 2> /dev/null | fzf +m) &&
+ cd "$dir"
+}
+
+fzfag() {
+  local dir
+  dir=$(ag --hiden ${1:-.} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
+
+}
+
+fzfcd() {
+    if [[ "$#" != 0 ]]; then
+        builtin cd "$@";
+        return
+    fi
+    while true; do
+        local lsd=$(echo ".." && ls -p | grep '/$' | sed 's;/$;;')
+        local dir="$(printf '%s\n' "${lsd[@]}" |
+            fzf --reverse --preview '
+                __cd_nxt="$(echo {})";
+                __cd_path="$(echo $(pwd)/${__cd_nxt} | sed "s;//;/;")";
+                echo $__cd_path;
+                echo;
+                ls -p --color=always "${__cd_path}";
+        ')"
+        [[ ${#dir} != 0 ]] || return 0
+        builtin cd "$dir" &> /dev/null
+    done
+}
+
 # TODO: put this in zshrc?
 # Use C-x C-e to edit the current command line
 autoload -U edit-command-line
