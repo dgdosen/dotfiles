fac2f984b830b38c423298c9db627a84374c955f
Daniel Dosen <dgdosen@gmail.com>
2020-12-22 19:18:28 -0800

    Update config.fish
    fish config changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,7 @@ set -gx PATH $PATH /Applications/Postgres.app/Contents/Versions/latest/bin
 # set -gx PATH (brew --prefix)/var/rbenv/shims $PATH
 
 # nodenv
-set -Ux fish_user_paths $HOME/.nodenv/bin $fish_user_paths
+set -gx fish_user_paths $HOME/.nodenv/bin $fish_user_paths
 set -gx PATH $HOME/.nodenv/shims $PATH
 
 # alias
@@ -22,7 +22,8 @@ complete --command c --arguments '(__fish_complete_directories ~/dev/)'
 
 # set -gx RBENV_ROOT /opt/homebrew/var/rbenv/
 set -gx RBENV_ROOT /usr/local/var/rbenv/
-set fish_plugins autojump vi-mode
+set fish_key_bindings fish_user_key_bindings
+# set fish_plugins autojump vi-mode
 
 set -g theme_nerd_fonts yes
 # theme_gruvbox dark medium
@@ -30,10 +31,11 @@ set -g theme_color_scheme gruvbox
 set -g theme_date_format "+%Y-%m-%d %H:%M %a"
 # set -g theme_color_scheme base16
 set -g theme_display_vi yes
-set -g theme_display_ruby yes
+set -g theme_display_ruby no
+# set -g theme_display_nvm yes
 
 set -g theme_nerd_fonts yes
-set -g theme_powerline_fonts yes
+# set -g theme_powerline_fonts yes
 # set -g theme_color_scheme solarized
 
 set -x fish_color_command green --bold
