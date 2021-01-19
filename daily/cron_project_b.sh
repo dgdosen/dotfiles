b4feb0f82823e9adef9f950caa215c1140eb8a86
Daniel Dosen <dgdosen@gmail.com>
2021-01-18 22:54:57 -0800

    update dotfile setting for postgres v13 default

--- daily/cron_project_b.sh
+++ daily/cron_project_b.sh
@@ -1,8 +1,8 @@
 #!/usr/local/bin/zsh
 source ~/.zshrc
 echo - updating project b data
-PGDATA=$HOME/Library/Application\ Support/Postgres/var-12 pg_ctl stop
-PGDATA=$HOME/Library/Application\ Support/Postgres/var-12 pg_ctl start
+PGDATA=$HOME/Library/Application\ Support/Postgres/var-13 pg_ctl stop
+PGDATA=$HOME/Library/Application\ Support/Postgres/var-13 pg_ctl start
 # PGDATA=/opt/homebrew/var/postgres pg_ctl stop
 # PGDATA=/opt/homebrew/var/postgres pg_ctl start
 echo - updating project b db
