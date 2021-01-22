2acd337d1816d3f006bb55ee96481490931a8ba6
Daniel Dosen <dgdosen@gmail.com>
2021-01-22 06:55:31 -0800

    wip: postgres

---
+++ daily/cron_recycle_postgres.sh
@@ -0,0 +1,7 @@
+#!/usr/local/bin/zsh
+source ~/.zshrc
+echo - recyling postgres
+PGDATA=$HOME/Library/Application\ Support/Postgres/var-13 pg_ctl stop
+PGDATA=$HOME/Library/Application\ Support/Postgres/var-13 pg_ctl start
+# PGDATA=/opt/homebrew/var/postgres pg_ctl stop
+# PGDATA=/opt/homebrew/var/postgres pg_ctl start
