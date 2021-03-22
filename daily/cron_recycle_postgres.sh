f7fda04f41151ee17806946652e4ac26527b0358
Daniel Dosen <dgdosen@gmail.com>
2021-03-22 05:01:17 -0700

    updated postgres recycling to one file (and change)

--- daily/cron_recycle_postgres.sh
+++ daily/cron_recycle_postgres.sh
@@ -1,7 +1,10 @@
 #!/usr/local/bin/zsh
 source ~/.zshrc
 echo - recyling postgres
-PGDATA=$HOME/Library/Application\ Support/Postgres/var-13 pg_ctl stop
-PGDATA=$HOME/Library/Application\ Support/Postgres/var-13 pg_ctl start
+# PGDATA=$HOME/Library/Application\ Support/Postgres/var-13 pg_ctl stop
+# PGDATA=$HOME/Library/Application\ Support/Postgres/var-13 pg_ctl start
 # PGDATA=/opt/homebrew/var/postgres pg_ctl stop
 # PGDATA=/opt/homebrew/var/postgres pg_ctl start
+brew services stop postgresql
+sleep 5
+brew services start postgresql
