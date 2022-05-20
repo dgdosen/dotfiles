f7792477a98cad5510c04e9a194a50b5905c9d43
Daniel Dosen <dgdosen@gmail.com>
2022-05-20 10:16:05 -0700

    wip: removing 'daily' project b setup from launchd

--- setup/launchd_setup.sh
+++ setup/launchd_setup.sh
@@ -2,8 +2,8 @@ lunchy install ~/.dotfiles/.launchd/com.agidevelopment.backup.plist
 lunchy install ~/.dotfiles/.launchd/com.agidevelopment.brewupdate.plist
 lunchy install ~/.dotfiles/.launchd/com.agidevelopment.dotupdate.plist
 lunchy install ~/.dotfiles/.launchd/com.agidevelopment.fetch.plist
-lunchy install ~/.dotfiles/.launchd/com.agidevelopment.project_b_db.plist
-lunchy install ~/.dotfiles/.launchd/com.agidevelopment.project_b_dw_db.plist
+# lunchy install ~/.dotfiles/.launchd/com.agidevelopment.project_b_db.plist
+# lunchy install ~/.dotfiles/.launchd/com.agidevelopment.project_b_dw_db.plist
 lunchy install ~/.dotfiles/.launchd/com.agidevelopment.touch.plist
 # lunchy install ~/.dotfiles/.launchd/com.agidevelopment.zettelupdate.plist
 # lunchy install ~/.dotfiles/.launchd/com.agidevelopment.beardailycreate.plist
