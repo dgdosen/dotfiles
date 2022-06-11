eb6bccd171ee8b343d5b630da9e9f14806c12a09
Daniel Dosen <dgdosen@gmail.com>
2022-06-11 16:53:28 -0700

    wip: cleaning up code for alacritty-themes

--- .zsh_customizations/color_aliases.sh
+++ .zsh_customizations/color_aliases.sh
@@ -1,17 +1,17 @@
 al () {
-  # appearance_light
+  appearance_light
 }
 
 appearance_light () {
   export APPEARANCE='light'
-  # alacritty-themes gruvbox-light
+  alacritty-themes gruvbox-light
 }
 
 ad () {
-  # appearance_dark
+  appearance_dark
 }
 
 appearance_dark () {
   export APPEARANCE='dark'
-  # alacritty-themes gruvbox-dark
+  alacritty-themes gruvbox-dark
 }
