2c08e4b119c4b2bfbf503e21c1b1bcb1aa2c5783
Daniel Dosen <dgdosen@gmail.com>
2023-01-11 20:34:25 -0600

    wip: telescope find_files with search_dirs

--- nvim_dgd/lua/setup/telescope.lua
+++ nvim_dgd/lua/setup/telescope.lua
@@ -31,4 +31,4 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 -- TODO: I can't figure out how to get Telescope search_dirs option to work in a call via vim.keymap.
--- vim.keymap.set('n', '<leader>gm', require('telescope.builtin').find_files(search_dirs="./app/models"), { desc = '[G]et [M]odel' })
+-- vim.keymap.set('n', '<leader>gm', require('telescope.builtin').find_files({ search_dirs = { "./app/models" }}), { desc = '[G]et [M]odel' })
