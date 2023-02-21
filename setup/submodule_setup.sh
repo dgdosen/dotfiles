7c162e04534151633a4088b1e2d3506ee05bc5cf
Daniel Dosen <dgdosen@gmail.com>
2023-02-20 22:47:24 -0800

    color scheme updates

--- setup/submodule_setup.sh
+++ setup/submodule_setup.sh
@@ -1,4 +1,12 @@
 # dotfile submodules
+# NOTE: I've been using tokyonights too... Need those colors in alacritty and tmux
+# alacritty
+# mkdir -p ~/.config/alacritty/themes
+# git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+# tmux.conf
+# set -g @plugin 'egel/tmux-gruvbox'
+# set -g @plugin "janoamaral/tokyo-night-tmux"
+#
 cd ~/.dotfiles
 git submodule add https://github.com/egel/tmux-gruvbox.git .tmux/plugins/tmux-gruvbox
 git submodule add https://github.com/tmux-plugins/tpm.git .tmux/plugins/tpm
