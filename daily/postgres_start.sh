d22b44f12c40bdfb3cc45eb819b1fc674de333a7
Daniel Dosen <dgdosen@gmail.com>
2023-07-07 13:42:06 -0700

    wip: config changes - note use of data dir for postgres

--- daily/postgres_start.sh
+++ daily/postgres_start.sh
@@ -1,8 +1,8 @@
-#!/usr/local/bin/zsh
+#!/opt/homebrew/bin/zsh
 source ~/.zshrc
 # PGDATA=$HOME/Library/Application\ Support/Postgres/var-13 pg_ctl stop
 # PGDATA=$HOME/Library/Application\ Support/Postgres/var-13 pg_ctl start
-# PGDATA=/opt/homebrew/var/postgres pg_ctl stop
+PGDATA=/opt/homebrew/var/postgres pg_ctl stop
 PGDATA=/opt/homebrew/var/postgres pg_ctl start
 # echo - updating project b db
 # $HOME/.dotfiles/.postgres/postgres_project_b_dw_reset.sh
