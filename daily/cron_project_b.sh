93bce454f37bcfe874d25ff5e2c047f53ff9b986
Daniel Dosen <dgdosen@gmail.com>
2023-11-26 06:44:02 -0600

    tweaks to postgres resets

--- daily/cron_project_b.sh
+++ daily/cron_project_b.sh
@@ -1,6 +1,10 @@
 #!/usr/local/bin/zsh
 source ~/.zshrc
 echo - updating project b data
+DB_NAME="project_b_development"
+DB_USER="postgres"
+SQL_CMD="SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$DB_NAME' AND pid <> pg_backend_pid();"
+psql -U $DB_USER -c "$SQL_CMD"
 # PGDATA=$HOME/Library/Application\ Support/Postgres/var-13 pg_ctl stop
 # PGDATA=$HOME/Library/Application\ Support/Postgres/var-13 pg_ctl start
 # PGDATA=/opt/homebrew/var/postgres pg_ctl stop
--- daily/cron_project_b_dw.sh
+++ daily/cron_project_b_dw.sh
@@ -1,10 +1,17 @@
 #!/usr/local/bin/zsh
 source ~/.zshrc
 echo - updating project b data
+
+# Replace 'your_database_name' and 'your_username' with actual database name and username
+DB_NAME="project_b_dw_development"
+DB_USER="postgres"
+SQL_CMD="SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$DB_NAME' AND pid <> pg_backend_pid();"
+psql -U $DB_USER -c "$SQL_CMD"
+
 # PGDATA=$HOME/Library/Application\ Support/Postgres/var-13 pg_ctl stop
 # PGDATA=$HOME/Library/Application\ Support/Postgres/var-13 pg_ctl start
-# PGDATA=/opt/homebrew/var/postgres pg_ctl stop
-# PGDATA=/opt/homebrew/var/postgres pg_ctl start
+# PGDATA=/opt/homebrew/var/postgresql@14 pg_ctl stop
+# PGDATA=/opt/homebrew/var/postgresql@14 pg_ctl start
 echo - updating project b db
 $HOME/.dotfiles/.postgres/postgres_project_b_dw_reset.sh
 touch ~/.cron_support/cron_project_b_dw.txt
