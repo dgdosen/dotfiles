b7bae79bcafdd1961d70ad92724aa7b73a33c73a
Daniel Dosen <dgdosen@gmail.com>
2023-11-26 11:54:43 -0600

    fix for gmax audit script

--- daily/project_b_gmax_audit.sh
+++ daily/project_b_gmax_audit.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/zsh
 # run the audit locally...
 source ~/.zshrc
-echo - running the project b gmax (and other?) audit for missing race data
+echo "running the project b gmax (and other?) audit for missing race data"
 cd $HOME/dev/project_b_api
 export RAILS_ENV=development
 bundle exec rake project_b:audit:json_for_missing_gmax_for_future_races
