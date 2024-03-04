f475b696c0f5cd71ff12f0975a4e65292d460443
Daniel Dosen <dgdosen@gmail.com>
2024-03-04 11:04:49 -0800

    added trouble plugin

--- nvim_dgd/lua/plugins.lua
+++ nvim_dgd/lua/plugins.lua
@@ -236,6 +236,16 @@ require('lazy').setup({
     opts = { signs = false }
   },
 
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    },
+  },
+
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
