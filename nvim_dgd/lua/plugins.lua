4b53ddd6b279391fec34c9da968f8f6ad241a2a2
Daniel Dosen <dgdosen@gmail.com>
2024-03-04 07:29:34 -0800

    plugin changes: todo-comment conform

--- nvim_dgd/lua/plugins.lua
+++ nvim_dgd/lua/plugins.lua
@@ -99,25 +99,25 @@ require('lazy').setup({
     dependencies = { "nvim-lua/plenary.nvim" }
   },
 
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    opts = {
-      notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
-      },
-    },
-  },
+  -- { -- Autoformat
+  --   'stevearc/conform.nvim',
+  --   opts = {
+  --     notify_on_error = false,
+  --     format_on_save = {
+  --       timeout_ms = 500,
+  --       lsp_fallback = true,
+  --     },
+  --     formatters_by_ft = {
+  --       lua = { 'stylua' },
+  --       -- Conform can also run multiple formatters sequentially
+  --       -- python = { "isort", "black" },
+  --       --
+  --       -- You can use a sub-list to tell conform to run *until* a formatter
+  --       -- is found.
+  --       -- javascript = { { "prettierd", "prettier" } },
+  --     },
+  --   },
+  -- },
 
 
   -- svelte (no lsp?
@@ -228,6 +228,13 @@ require('lazy').setup({
   -- 'mg979/vim-visual-multi',
   -- 'gcmt/wildfire.vim',
   -- 'tpope/vim-surround',
+
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false }
+  },
 
   -- Git related plugins
   'tpope/vim-fugitive',
