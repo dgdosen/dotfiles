853ce8a08866a50075c0dc35930c087decc2e98f
Daniel Dosen <dgdosen@gmail.com>
2024-03-06 10:04:46 -0800

    updated comments.nvim defaults

--- nvim_dgd/lua/setup/comment.lua
+++ nvim_dgd/lua/setup/comment.lua
@@ -1,2 +1,9 @@
 -- Enable Comment.nvim
-require('Comment').setup()
+require('Comment').setup {
+  mappings = {
+    ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+    basic = true,
+    ---Extra mapping; `gco`, `gcO`, `gcA`
+    extra = true,
+  },
+}
