2e905cc91cdd17145d9ce3f03f176b11e8200ddb
Daniel Dosen <dgdosen@gmail.com>
2024-03-20 14:43:31 -0700

    updated ruby telescope options

--- nvim_dgd/lua/setup/ruby.lua
+++ nvim_dgd/lua/setup/ruby.lua
@@ -16,6 +16,8 @@ end
 vim.keymap.set('n', '<leader>sym', function() search_in_folder("app/models") end, { desc = '[s]earch rub[y] [m]odels' })
 vim.keymap.set('n', '<leader>syc', function() search_in_folder("spec") end, { desc = '[s]earch rub[y] spe[c]' })
 vim.keymap.set('n', '<leader>syo', function() search_in_folder("app/objects") end, { desc = '[s]earch rub[y] [o]bjects' })
+vim.keymap.set('n', '<leader>syz', function() search_in_folder("app/serializers/api/v2") end,
+  { desc = '[s]earch rub[y] seriali[z]ers' })
 vim.keymap.set('n', '<leader>sys', function() search_in_folder("app/services") end,
   { desc = '[s]earch rub[y] [s]ervices' })
 vim.keymap.set('n', '<leader>syt', function() search_in_folder("app/types") end,
