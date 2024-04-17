fd45e2c760f3ad851b7c1c2d59edf5614b6ea9ec
Daniel Dosen <dgdosen@gmail.com>
2024-04-17 01:37:46 -0700

    updated hchoa sql

--- local/share/db_ui/hchoa_dev/transactions.sql
+++ local/share/db_ui/hchoa_dev/transactions.sql
@@ -1,14 +1,49 @@
-select pt.posting_date, pt.transactionable_id, pt.transactionable_type, pt.memo,
+select e.id, e.name, e.customer_id, i.number, i.customer_id, a.id as account_id, a.name,
+pt.posting_date, pt.transactionable_id, pt.transactionable_type, pt.memo,
 p.credit_amount, p.debit_amount
-from posting_transactions as pt, postings as p
+from posting_transactions as pt,
+postings as p,
+invoices as i,
+accounts as a,
+customers as c,
+entities as e
 where p.posting_transaction_id = pt.id
+and pt.transactionable_id = i.id
+and i.customer_id = c.id
+and c.account_id = a.id
+and e.customer_id = c.id
 order by pt.created_at desc
+limit 40;
+
+select * from postings
+order by id desc
 limit 10;
 
-select * from postings limit 10;
-select * from posting_transactions limit 10;
+select * from posting_transactions
+order by id desc
+limit 25;
 
 select * from financial_accounts limit 100;
+
+select * from entities
+order by id desc
+limit 10;
+
+select * from invoices
+order by id desc
+limit 10;
+
+select * from contacts
+order by id desc
+limit 10;
+
+select * from customers
+order by id desc
+limit 30;
+
+select * from accounts
+order by id desc
+limit 10;
 
 select * from accounts
 where description = 'Customer Account'
