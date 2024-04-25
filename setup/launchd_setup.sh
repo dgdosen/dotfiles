28c574daa0afc4a76978b8bd023840871c9483b9
Daniel Dosen <dgdosen@gmail.com>
2024-04-25 14:01:36 -0700

    setup updates from manistee v2

--- setup/launchd_setup.sh
+++ setup/launchd_setup.sh
@@ -1,9 +1,24 @@
 lunchy install ~/.dotfiles/.launchd/com.agidevelopment.backup.plist
+lunchy install ~/.dotfiles/.launchd/com.agidevelopment.beardailyupdate.plist
+lunchy install ~/.dotfiles/.launchd/com.agidevelopment.bearweeklycreate.plist
 lunchy install ~/.dotfiles/.launchd/com.agidevelopment.brewupdate.plist
 lunchy install ~/.dotfiles/.launchd/com.agidevelopment.dotupdate.plist
+lunchy install ~/.dotfiles/.launchd/com.agidevelopment.logrotate.plist
 lunchy install ~/.dotfiles/.launchd/com.agidevelopment.fetch.plist
-# lunchy install ~/.dotfiles/.launchd/com.agidevelopment.project_b_db.plist
-# lunchy install ~/.dotfiles/.launchd/com.agidevelopment.project_b_dw_db.plist
 lunchy install ~/.dotfiles/.launchd/com.agidevelopment.touch.plist
-# lunchy install ~/.dotfiles/.launchd/com.agidevelopment.zettelupdate.plist
-# lunchy install ~/.dotfiles/.launchd/com.agidevelopment.beardailycreate.plist
+
+lunchy install ~/.dotfiles/.launchd/com.agidevelopment.project_b_db.plist
+lunchy install ~/.dotfiles/.launchd/com.agidevelopment.project_b_dw_db.plist
+
+lunchy start com.agidevelopment.backup
+lunchy start com.agidevelopment.beardailyupdate
+lunchy start com.agidevelopment.bearweeklycreate
+lunchy start com.agidevelopment.brewupdate
+lunchy start com.agidevelopment.dotupdate
+lunchy start com.agidevelopment.logrotate
+lunchy start com.agidevelopment.fetch
+lunchy start com.agidevelopment.touch
+
+lunchy start com.agidevelopment.project_b_db
+lunchy start com.agidevelopment.project_b_dw_db
+
