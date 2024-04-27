3126f9f948a37b61b5b376bb0bf42e8bbd470214
Daniel Dosen <dgdosen@gmail.com>
2024-04-27 11:10:44 -0700

    updating launchd default

--- .zsh_customizations/environment_aliases.sh
+++ .zsh_customizations/environment_aliases.sh
@@ -15,7 +15,7 @@ nvimup() {
   echo " -- Lazy Updated"
   nvim --headless -c "TSUpdate" +qa
   echo " -- TreeSitter Updated"
-  nvim --headless -c "MasonUpdateAll" 
+  nvim --headless -c "MasonUpdateAll"
   # nvim --headless -c 'UpdateRemotePlugins' +qa
   echo " -- Mason Updated"
   cd $FOO
@@ -95,11 +95,35 @@ fzfcd() {
     done
 }
 
+function reload_agidevelopment_plists2() {
+  local plist_dir="${1/#\~/$HOME}"  # Expands ~ to $HOME
+  local match_string="com.agidevelopment"
+
+  # Find all plist files in the specified directory that contain 'com.agidevelopment'
+  # The -maxdepth 1 option prevents find from searching subdirectories
+  find "$plist_dir" -maxdepth 1 -name "*$match_string*.plist" | while read plist_path; do
+    echo "Reloading $plist_path"
+    local service_name="$(basename "$plist_path" .plist)"
+    launchctl unload "$plist_path" 2>/dev/null
+    launchctl load "$plist_path"
+  done
+}
+
+function reload_all_agidevelopment_plists() {
+  local plist_dir="$1"
+
+  # Find all plist files in the specified directory that contain 'com.agidevelopment'
+  find "$plist_dir" -name "*com.agidevelopment*.plist" | while read plist_path; do
+    echo "Setting up monitoring for $plist_path"
+    monitor_and_reload_plist "$plist_path" &
+  done
+}
+
 dltest() {
   date  ## echo the date at start
   # the script contents
   curl https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-desktop-amd64.iso --output ~/downloads/ubuntutest.iso
-  date  ## echo the date at enda    
+  date  ## echo the date at enda
 }
 
 # TODO: put this in zshrc?
