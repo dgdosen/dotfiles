bb3b27b22c91d2b2636eba2e91af5fd2d637ebb7
Daniel Dosen <dgdosen@gmail.com>
2024-10-24 09:15:32 -0500

    neovim color updates

---
+++ nvim_dgd/lua/setup/nvim-highlight-colors.lua
@@ -0,0 +1,9 @@
+require('nvim-highlight-colors').setup({
+  ---Render style
+  ---@usage 'background'|'foreground'|'virtual'
+  render = 'virtual',
+
+  ---Set virtual symbol (requires render to be set to 'virtual')
+  virtual_symbol = '\u{25CF}',
+
+})
--- nvim_dgd/lua/setup/todo-comments.lua
+++ nvim_dgd/lua/setup/todo-comments.lua
@@ -1,12 +1,27 @@
--- FIX: test
--- TODO: test
-
+-- FIX: foobar
+-- PERF: foobar - this should be purple
+-- HACK: foobar
 -- DGD: foobar
+-- TODO: foobar
+-- NOTE: foobar
+-- DOC: foobar - easy on the eyes!
+-- TEST: foobar
+-- def_purple = { "", "#B16286" }
 
 require("todo-comments").setup(
   {
     keywords = {
       DGD = { color = "warning" },
+      DOC = { color = "def_grey" },
+      PERF = { color = "def_purple" },
+      TODO = { color = "def_orange" }
+
+    },
+    colors = {
+      def_white = { "", "#EBDBB2" },
+      def_grey = { "", "#A89984" },
+      def_purple = { "", "#D3869B" },
+      def_orange = { "", "#fe8019" }
     },
     merge_keywords = true,
   }
