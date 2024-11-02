c1195495dfffbde1935993f900463328da529801
Daniel Dosen <dgdosen@gmail.com>
2024-11-01 22:35:54 -0700

    updates to daily nvim update

--- daily/nvim_daily.sh
+++ daily/nvim_daily.sh
@@ -1,5 +1,16 @@
 #!/usr/local/bin/zsh
 source ~/.zshrc
+
+# Run Lazy sync
 nvim --headless '+Lazy! sync' +qa
+
+# Run MasonUpdate and wait until it's complete
+# nvim --headless -c 'lua require("mason-registry").update(function() vim.cmd("qa") end)'
+
+# Run MasonUpdateAll and wait until it's complete
+# nvim --headless -c 'lua require("mason-update-all").update_all_packages(function() vim.cmd("qa") end)'
+
+# nvim --headless '+Lazy! sync' +qa
 nvim --headless '+MasonUpdate' +qa
 nvim --headless '+MasonUpdateAll' +qa
+sleep 10
