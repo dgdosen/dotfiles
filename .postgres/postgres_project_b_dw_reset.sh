8a822be7327af51f015f47dd84fdfdcaf9f0bbb6
Daniel Dosen <dgdosen@gmail.com>
2025-01-24 10:12:40 -0800

    wip: working with different dropbox roots

--- .postgres/postgres_project_b_dw_reset.sh
+++ .postgres/postgres_project_b_dw_reset.sh
@@ -7,9 +7,9 @@ createdb -U postgres -h localhost project_b_dw_development
 
 echo restoring
 date
-file=$(ls -t $HOME/dropboxm/joined_shares/project_b_share/pg_dw_database_backup/project_b* | head -1)
+file=$(ls -t "$DROPBOX_BUSINESS/Daniel Dosen/joined_shares/project_b_share/pg_dw_database_backup/"project_b* | head -1)
 echo $file
-pg_restore $file -U postgres -d project_b_dw_development -h localhost -Fc >> /dev/null
+pg_restore "$file" -U postgres -d project_b_dw_development -h localhost -Fc >> /dev/null
 # pg_restore $file -U postgres -d project_b_dw_development -h localhost -Fc > /dev/null
 date
 echo finished restoring
