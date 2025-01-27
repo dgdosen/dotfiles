320eeb3b01adbcffaa1df63d5ec3a108572c85b8
Daniel Dosen <dgdosen@gmail.com>
2025-01-27 08:20:20 -0800

    using dynamic DROPBOX locations in scripts

--- .postgres/postgres_project_b_reset.sh
+++ .postgres/postgres_project_b_reset.sh
@@ -7,9 +7,9 @@ createdb -U postgres -h localhost project_b_development
 
 echo restoring
 date
-file=$(ls -t $HOME/dropboxm/joined_shares/project_b_share/pg_database_backup/project_b* | head -1)
+file=$(ls -t "$DROPBOX_BUSINESS/Daniel Dosen/joined_shares/project_b_share/pg_database_backup/"project_b* | head -1)
 echo $file
-pg_restore $file -U postgres -d project_b_development -h localhost -j 1 -Fc > /dev/null
+pg_restore "$file" -U postgres -d project_b_development -h localhost -Fc >> /dev/null
 # pg_restore $file -U postgres -d project_b_development -h localhost -j 3 -Fc >> restore.log 2>&1
 # pg_restore $file -U postgres -d project_b_development -h localhost -Fc >> restore.log 2>&1
 # pg_restore $file -U postgres -d project_b_development -h localhost -Fc > /dev/null
