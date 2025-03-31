df435eddf5d3d012eb84a88f78c5e362676ddacc
Daniel Dosen <dgdosen@gmail.com>
2025-03-31 07:08:53 -0700

    updated drf fetch processing

--- daily/projectb_drf_fetch.sh
+++ daily/projectb_drf_fetch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 source ~/.zshrc
 cd $HOME/dev/project_b_drf_scrape_cli
-yarn dev
+yarn start scrape_drf_data
+yarn start download_drf_files
 
 touch ~/.cron_support/cron_project_drf_scrape.txt
