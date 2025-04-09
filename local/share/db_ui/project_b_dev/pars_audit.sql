0df5b0a8e63757c4817a1bec45ce0c330cec63b1
Daniel Dosen <dgdosen@gmail.com>
2025-04-09 07:31:38 -0700

    par/race_fact sql updates

--- local/share/db_ui/project_b_dev/pars_audit.sql
+++ local/share/db_ui/project_b_dev/pars_audit.sql
@@ -1,2 +1,27 @@
 -- sql for auditing completeness of pars (and race fact) data
 
+select distinct races.track_code, distinct races.date, distinct races.race_number
+from races, egps_fractions where
+races.id = egps_fractions.race_id
+and races.track_code in ('SA', 'DMR', 'CD')
+order by track_code, date
+
+select distinct races.track_code, distinct races.date, distinct races.race_number
+from races, egps_fractions where
+races.id = egps_fractions.race_id
+and races.track_code in ('SA', 'DMR', 'CD')
+order by track_code, date
+
+SELECT DISTINCT1
+  races.track_code,
+  races.date,
+  races.race_number
+FROM
+  races
+JOIN
+  egps_fractions ON races.id = egps_fractions.race_id
+WHERE
+  races.track_code IN ('SA', 'DMR', 'CD')
+ORDER BY
+  races.track_code,
+  races.date
