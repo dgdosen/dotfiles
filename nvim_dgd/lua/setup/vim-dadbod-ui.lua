903815240aa855a5c468822e718dcb9e2ba0fda5
Daniel Dosen <dgdosen@gmail.com>
2025-05-09 08:48:35 -0500

    updated dadbod-ui support

--- nvim_dgd/lua/setup/vim-dadbod-ui.lua
+++ nvim_dgd/lua/setup/vim-dadbod-ui.lua
@@ -2,6 +2,13 @@
 -- nothing to see here
 vim.g['db_ui_win_position'] = 'right'
 vim.g['db_ui_use_nerd_fonts'] = 1
--- vim.g['db_ui_save_location'] = '~/dev/project_b_api/db/sql'
 vim.g['db_ui_execute_on_save'] = 0
 vim.g['db_ui_use_nerd_fonts'] = 1
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "dbui", "sql" },
+  callback = function()
+    vim.keymap.set("n", "<leader>r", "<Plug>(DBUI_ExecuteQuery)", { buffer = true, remap = true })
+    vim.keymap.set("v", "<leader>r", "<Plug>(DBUI_ExecuteQuery)", { buffer = true, remap = true })
+  end,
+})
