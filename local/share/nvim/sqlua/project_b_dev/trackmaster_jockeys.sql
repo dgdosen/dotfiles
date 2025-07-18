aecc496646257ab9a2104e0c231c2c5bc52fd46d
Daniel Dosen <dgdosen@gmail.com>
2025-07-17 21:22:17 -0700

    update to jockey query

---
+++ local/share/nvim/sqlua/project_b_dev/trackmaster_jockeys.sql
@@ -0,0 +1,17 @@
+select * from trackmaster_jockeys 
+order by last_name, first_name, middle_name
+limit 1000;
+
+select * from jockeys 
+order by last_name, first_name, middle_name
+limit 1000;
+
+
+
+select * from trackmaster_trainers 
+order by last_name, first_name, middle_name
+limit 1000;
+
+select * from trainers
+order by last_name, first_name, middle_name
+limit 1000;
