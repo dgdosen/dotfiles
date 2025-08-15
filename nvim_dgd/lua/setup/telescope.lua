c78b04dd7082a82a5efb31b21371437f42cf9440
Daniel Dosen <dgdosen@gmail.com>
2025-08-14 19:39:15 -0700

    updated telescope searching

--- nvim_dgd/lua/setup/telescope.lua
+++ nvim_dgd/lua/setup/telescope.lua
@@ -33,13 +33,19 @@ local builtin = require 'telescope.builtin'
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[s]earch [h]elp' })
 vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[s]earch [k]eymaps' })
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[s]earch [f]iles' })
-vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[s]earch [s]elect Telescope' })
+vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[s]earch Telescope [s]ymbols' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[s]earch current [w]ord' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[s]earch by [g]rep' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[s]earch [d]iagnostics' })
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[s]earch [r]esume' })
 vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[s]earch Recent Files ("." for repeat)' })
 vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+vim.keymap.set('n', '<leader>sso', builtin.lsp_document_symbols, { desc = '[s]earch [s]ymbols in [o]pen file' })
+vim.keymap.set('n', '<leader>ssw', builtin.lsp_workspace_symbols, { desc = '[s]earch [s]ymbols in [w]orkspace' })
+vim.keymap.set('n', '<leader>ssd', builtin.lsp_dynamic_workspace_symbols,
+  { desc = '[s]earch [s]ymbols [d]ynamically in workspace' })
+vim.keymap.set('n', '<leader>sst', builtin.treesitter, { desc = '[s]earch [s]ymbols with [t]reesitter (file outline)' })
 
 vim.keymap.set('n', '<leader>s/', function()
   builtin.live_grep {
