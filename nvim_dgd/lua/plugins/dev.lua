767a5b1826442ad1af070ea62ef113ddfed50fe9
Daniel Dosen <dgdosen@gmail.com>
2025-09-08 10:53:13 -0500

    added dev plugin loading

--- nvim_dgd/lua/plugins/dev.lua
+++ nvim_dgd/lua/plugins/dev.lua
@@ -1,15 +1,15 @@
 local plugins = {}
 
 -- adding local dev of nvim plugins (if exists on current machine)
--- if vim.fn.isdirectory(vim.fn.expand("~/dev/plugins/present.nvim")) == 1 then
---   table.insert(plugins, {
---     "present.nvim",
---     dir = "~/dev/plugins/present.nvim",
---     dev = true,
---     config = function()
---       require("present").setup()
---     end,
---   })
--- end
+if vim.fn.isdirectory(vim.fn.expand("~/dev/plugins/present.nvim")) == 1 then
+  table.insert(plugins, {
+    "present.nvim",
+    dir = "~/dev/plugins/present.nvim",
+    dev = true,
+    config = function()
+      require("present").setup()
+    end,
+  })
+end
 
 return plugins
