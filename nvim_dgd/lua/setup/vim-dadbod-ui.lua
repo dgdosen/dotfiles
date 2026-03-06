1cd4e00180aeaa10c73578a0a38f896f36cf56b1
Daniel Dosen <dgdosen@gmail.com>
2026-03-05 19:21:05 -0800

    Document usage of DBUI

--- nvim_dgd/lua/setup/vim-dadbod-ui.lua
+++ nvim_dgd/lua/setup/vim-dadbod-ui.lua
@@ -4,6 +4,9 @@ vim.g['db_ui_win_position'] = 'right'
 vim.g['db_ui_use_nerd_fonts'] = 1
 vim.g['db_ui_execute_on_save'] = 0
 vim.g['db_ui_use_nerd_fonts'] = 1
+-- vim.g['db_ui_save_location'] = '~/.local/share/dbui'
+-- this next line works, but is achieved by a symlink we have in .local
+-- vim.g['db_ui_save_location'] = '~/.dotfiles/local/share/nvim/sqlua'
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "dbui", "sql" },
