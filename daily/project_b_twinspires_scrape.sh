83655f01b0ba9c9f82f1254c455956f73fb547a7
Daniel Dosen <dgdosen@gmail.com>
2026-05-04 17:19:00 -0700

    Update twinspires odds fetcher cli

--- daily/project_b_twinspires_scrape.sh
+++ daily/project_b_twinspires_scrape.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 source ~/.zshrc
 
-cd $HOME/dev/project_b_twinspires_scrape_cli
+cd $HOME/dev/project_b_twinspires_odds_scrape_cli
 yarn dev
 
 touch ~/.cron_support/cron_project_b_twinspires_scrape.txt
