98409bf39801be02222ce58067c1c24053dc9c33
Daniel Dosen <dgdosen@gmail.com>
2026-05-06 21:45:12 -0700

    Update daily script to call right cli commands

--- daily/project_b_progressive_breeding_scrape.sh
+++ daily/project_b_progressive_breeding_scrape.sh
@@ -5,9 +5,12 @@ cd $HOME/dev/project_b_progressive_breeding_scrape_cli
 
 pnpm build
 
-pnpm start fetch -t sa
-pnpm start fetch -t cd
-pnpm start fetch -t dmr
-pnpm start process
+pnpm dev fetch
+pnpm dev process
+
+# pnpm start fetch -t sa
+# pnpm start fetch -t cd
+# pnpm start fetch -t dmr
+# pnpm start process
 
 touch ~/.cron_support/cron_project_b_progressive_breeding_scrape.txt
