6ca6d8e4324d4ed3a8c85a3b207cbb8fb3d51654
Daniel Dosen <dgdosen@gmail.com>
2026-05-21 09:49:10 -0700

    Add launch agent for twinspires data fetch

---
+++ daily/project_b_twinspires_data_scrape.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+source ~/.zshrc
+
+cd $HOME/dev/project_b_twinspires_data_scrape_cli
+pnpm exec tsx src/index.ts fetch-all
+
+touch ~/.cron_support/cron_project_b_twinspires_data_scrape.txt
